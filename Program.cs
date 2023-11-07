b05f23ca82600c3d02225a691bc35f6af8a677a8
kgibbsdev <54226327+kgibbsdev@users.noreply.github.com>
2023-11-06 22:20:07 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,42 @@
+using System.Net;
+using System.Net.Mail;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+var summaries = new[]
+{
+    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+};
+
+app.MapGet("/send", () =>
+{
+    SmtpClient client = new SmtpClient("smtp.gmail.com", 587);
+    
+    client.UseDefaultCredentials = false;
+    NetworkCredential creds = new NetworkCredential("email", "password");
+})
+.WithName("SendEmail")
+.WithOpenApi();
+
+app.Run();
+
+internal record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
+{
+    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+}
